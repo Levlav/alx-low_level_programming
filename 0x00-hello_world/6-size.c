b4750bdded81f92e56f58cc092127f8aa57e3c6a
Levlav <levinlavatsa@gmail.com>
2023-03-16 15:36:30 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include < studio.h >
+/**
+*main -Entry
+*Return:Always 0
+*/
+int main(void)
+{
+	printf(“size of char : % i byte(s)\n”, sizeof(char);
+	printf(“size of an int : % i byte(s)\n”, sizeof(int);
+	printf(“size of longint : % i byte(s)\n”, sizeof(longint);
+	printf(“size a float : % i byte(s)\n”, sizeof(float);
+return (0);
+}
+
